3e615a2da7fb3cc4e0b7c93c2bf83c40fccbfffb
Ismael <Ismael.Tortosa@hotmail.com>
2023-10-10 09:59:28 +0200

    HeadAndTails

---
+++ wear/src/test/java/ismaApp/tortosa/fieldraffle/MainActivityTest.java
@@ -0,0 +1,21 @@
+package ismaApp.tortosa.fieldraffle;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Test
+    public void test(){
+        int num1 = 1;
+        int num2 = 1;
+
+        assertEquals(num1, num2);
+    }
+
+}
+
+
+
+
+
